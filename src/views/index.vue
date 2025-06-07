78c3e8e99152dd3b55f9a3b297584a065f661a36
blandh <blandh26@gmail.com>
2025-06-07 22:53:52 +0800

    불필요한 코드 주석

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,40 +2,6 @@
   <div class="app-container home">
     <noticeBar :dataList="noticeList"> </noticeBar>
     <el-row :gutter="20">
-      <el-col :sm="24" :lg="24">
-        <blockquote class="text-warning" style="font-size: 14px">
-          【阿里云特惠专区】
-          <el-link href="https://www.aliyun.com/minisite/goods?userCode=uotn5vt1&share_source=copy_link" type="primary" target="_black">
-            ☛☛点我进入☚☚
-          </el-link>
-          <br />
-          【领取腾讯云通用云产品新用户专属大礼包2860优惠券，每种代金券限量500张，先到先得。】
-          <el-link href="https://curl.qcloud.com/5J4nag8D" type="primary" target="_blank"> ☛☛点我进入☚☚ </el-link>
-          <br />
-
-          【腾讯云限时秒杀活动】
-          <el-link href="https://curl.qcloud.com/4yEoRquq" type="primary" target="_blank">☛☛点我进入☚☚ </el-link>
-          <br />
-
-          【华为特惠专区，多款产品限时特价】
-          <el-link
-            href="https://activity.huaweicloud.com/discount_area_v5/index.html?fromacct=a53709d1-149d-49f4-9b89-bf62bd96ef65&utm_source=aGlkX3N0dnZkMWNxejBnOTJ5OA===&utm_medium=cps&utm_campaign=201905"
-            type="primary"
-            target="_blank">
-            ☛☛点我进入☚☚
-          </el-link>
-          <br />
-          【领取七牛云通用云产品优惠券】
-          <el-link href="https://s.qiniu.com/FzEfay" type="primary" target="_blank"> ☛☛点我进入☚☚ </el-link>
-          <br />
-          【Gitee 企业版优惠专区】
-          <el-link href="https://gitee.com/enterprises?invite_code=Z2l0ZWUtMTI1NzM1OQ%3D%3D" type="primary" target="_blank"> ☛☛点我进入☚☚ </el-link>
-          <br />
-          <h4 class="text-danger">云产品通用红包，可叠加官网常规优惠使用。(仅限新用户)</h4>
-        </blockquote>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20">
       <el-col :lg="16" :sm="24">
         <h2>ZRAdmin.NET {{ $t('layout.backstageManagement') }}</h2>
         <p>
@@ -43,7 +9,7 @@
           <code>vue页面</code>主要使用了若依，后端参考Ruoyi SpringBoot版本，在此表示感谢.)
         </p>
         <p>{{ $t('layout.content1') }}</p>
-        <p>
+        <!-- <p>
           <b>{{ $t('layout.currentVersion') }}:</b> <span>v{{ version }}</span>
           <el-link
             class="ml10"
@@ -54,15 +20,15 @@
             @click="goTarget('http://www.izhaorui.cn/doc/changelog.html#' + version)"
             >{{ $t('layout.changeLog') }}
           </el-link>
-        </p>
-        <p>
+        </p> -->
+        <!-- <p>
           <el-button type="primary" size="small" icon="Cloudy" plain @click="goTarget('https://gitee.com/izory/ZrAdminNetCore')">Gitee </el-button>
           <el-button type="primary" size="small" icon="Cloudy" plain @click="goTarget('https://github.com/izhaorui/ZrAdmin.NET')"
             >Github
           </el-button>
-        </p>
+        </p> -->
         <p></p>
-        <h3>{{ $t('layout.tip1') }}</h3>
+        <!-- <h3>{{ $t('layout.tip1') }}</h3> -->
       </el-col>
       <el-col :lg="8" :sm="24">
         <h2>{{ $t('layout.technicalSelection') }}</h2>
@@ -98,69 +64,6 @@
       </el-col>
     </el-row>
     <el-divider />
-    <el-row :gutter="20" class="mt10">
-      <el-col :sm="24" :lg="8">
-        <el-card>
-          <template #header>
-            <span>{{ $t('layout.donationSupport') }}</span>
-          </template>
-
-          <div class="body">
-            <div style="color: red">{{ $t('layout.rewardUser') }}</div>
-            <img src="@/assets/images/reward.jpg" alt="donate" style="width: 100%" />
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :sm="24" :lg="10">
-        <el-card>
-          <template #header>
-            <span>
-              移动端体验
-              <span style="color: red">如有需要联系作者</span>
-            </span>
-          </template>
-
-          <div class="body">
-            <table style="width: 100%; text-align: center">
-              <tbody>
-                <tr>
-                  <td>微信小程序</td>
-                  <td>H5</td>
-                </tr>
-                <tr>
-                  <td>
-                    <img src="@/assets/images/qrcode.jpg" alt="donate" style="width: 160px" />
-                  </td>
-                  <td>
-                    <img src="@/assets/images/qrcodeH5.png" alt="donate" style="width: 160px" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :sm="24" :lg="6">
-        <el-card>
-          <template #header>
-            <span>{{ $t('layout.contactUs') }}</span>
-          </template>
-          <div class="body">
-            <p>
-              <el-icon> <promotion /> </el-icon>{{ $t('layout.officialWebsite') }}：
-              <el-link href="http://www.izhaorui.cn" target="_blank"> http://www.izhaorui.cn </el-link>
-            </p>
-            <p>
-              <el-icon> <UserFilled /> </el-icon>{{ $t('layout.qqGroup') }}：
-              <s>满191349103</s>
-              <s>满462862273</s>
-              <s>满658227537</s>
-              <el-link target="_black" href="https://qm.qq.com/q/e8uHl134qc"> &nbsp;1026662023 </el-link>
-            </p>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
   </div>
 </template>
 
